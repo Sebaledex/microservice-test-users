82c76766cc603385ac655b64129d914fe2bac874
Sebaledex <91289240+Sebaledex@users.noreply.github.com>
2024-11-03 19:44:28 -0300

    Test users
    test users

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  //Importamos el module
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true,
+    }),
+    //Conexion con mongo
+    MongooseModule.forRoot(process.env.URI_MONGODB),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
